2eef5212ea044203be0167c7b0547cec7c979407
Thiago <bussoladev@gmail.com>
2024-07-13 16:26:46 -0300

    feat: basic app fluuter

---
+++ lib/heading.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Heading extends StatelessWidget {
+  const Heading(this.text, {super.key});
+  final String text;
+
+  @override
+  Widget build(context) {
+    return Text(
+      text,
+      style: const TextStyle(
+        color: Colors.white,
+        fontSize: 28,
+      ),
+    );
+  }
+}
